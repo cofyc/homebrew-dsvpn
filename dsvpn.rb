f1cf0c2ba208e82d1de332a580419c366a187610
Naveen Nathan <naveen@lastninja.net>
2019-08-05 07:02:42 +1000

    bumped to release 0.1.2

--- dsvpn.rb
+++ dsvpn.rb
@@ -2,8 +2,8 @@ class Dsvpn < Formula
   desc "Dead Simple VPN"
   homepage "https://github.com/jedisct1/dsvpn"
   url "https://github.com/jedisct1/dsvpn.git",
-      :tag      => "0.1.1",
-      :revision => "5101a86c3d0bfd387a0a90fc395bef5b4bb7ea0f"
+      :tag      => "0.1.2",
+      :revision => "ca0b69f47a08d9561f6c7d644f902201f39a37e3"
 
   def install
     system "make"
